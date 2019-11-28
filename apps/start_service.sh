5be7ad1cb536f1007ca9deeb85de4bf1067562f8
root <root@vagrant.vm>
2019-11-28 18:35:15 +0000

    ajuste prometheus

--- apps/start_service.sh
+++ apps/start_service.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 python3 /code/service.py &
-envoy -c /etc/service-envoy.yaml -l info --service-cluster service_${SERVICE_NAME} --service-node service_${SERVICE_NAME} --log-format '[METADATA][%Y-%m-%d %T.%e][%t][%l][%n] %v'
+envoy -c /etc/service-envoy.yaml --service-cluster service_${SERVICE_NAME} --service-node service_${SERVICE_NAME}
+
