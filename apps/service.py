d5a59e630599af028a8924bb27805f2d7b161033
bbernardo <bernowly@gmail.com>
2019-11-29 15:02:34 +0000

    ajuste adicao servico

--- apps/service.py
+++ apps/service.py
@@ -9,10 +9,20 @@ app = Flask(__name__)
 
 @app.route('/service/<service_id>')
 def service(service_id):
+    if (os.environ['SERVICE_NAME']) == 'a':
+        ret = requests.get("http://localhost:9000/trace/b")
+
     return ('Hello from {} (hostname: {} resolvedhostname:{})\n'.format(
                     os.environ['SERVICE_NAME'],
                     socket.gethostname(),
                     socket.gethostbyname(socket.gethostname())))
+
+@app.route('/trace/<service_id>')
+def trace(service_id):
+    if (os.environ['SERVICE_NAME']) == 'a':
+        ret = requests.get("http://localhost:9000/trace/b")
+
+    return service(service_id)
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8080, debug=True)
--- apps/start_service.sh
+++ apps/start_service.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 python3 /code/service.py &
-envoy -c /etc/service-envoy.yaml --service-cluster service_${SERVICE_NAME} --service-node service_${SERVICE_NAME}
+envoy -c /etc/service-${SERVICE_NAME}-envoy.yaml --service-cluster service_${SERVICE_NAME} --service-node service_${SERVICE_NAME}
 
